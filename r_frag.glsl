36d79456d4fbfdf048cd41134dfd5201cb46609c
Jasper St. Pierre <jstpierre@mecheye.net>
2016-03-22 10:58:52 -0700

    update w/ better names and demo pages

--- r_frag.glsl
+++
@@ -1,9 +0,0 @@
-
-precision mediump float;
-
-varying vec3 v_position;
-
-void main() {
-    vec3 color_pos = abs(v_position) / 2.0;
-    gl_FragColor = vec4(color_pos.x, color_pos.y, color_pos.z, 0.6);
-}
